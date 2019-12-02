a3a219c05402758df79e765efaa180b9dd189232
joeruiz1 <fernandostiven14yo@gmail.com>
2019-12-02 00:55:26 -0500

    4 files modified

--- src/app/registrar-encuestador/registrar-encuestador.component.ts
+++ src/app/registrar-encuestador/registrar-encuestador.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-registrar-encuestador',
   templateUrl: './registrar-encuestador.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarEncuestadorComponent implements OnInit {
 
-  constructor() { }
+      public id: string;
+  constructor(private route: ActivatedRoute) {
+  route.params.subscribe(
+    params=>{this.id=params['id']})
+  }
 
   ngOnInit() {
   }
--- src/app/visualizar-encuestas/visualizar-encuestas.component.ts
+++ src/app/visualizar-encuestas/visualizar-encuestas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-visualizar-encuestas',
   templateUrl: './visualizar-encuestas.component.html',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizarEncuestasComponent implements OnInit {
 
-  constructor() { }
-
+    public id: string;
+  constructor(private route: ActivatedRoute) {
+  route.params.subscribe(
+    params=>{this.id=params['id']})
+  }
   ngOnInit() {
   }
 
