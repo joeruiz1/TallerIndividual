bd2930124d3d1e08f9f46cafdc9415ae3a87353e
joeruiz1 <fernandostiven14yo@gmail.com>
2019-12-02 01:30:51 -0500

    2 files modified

--- src/app/registrar-encuestador/registrar-encuestador.component.ts
+++ src/app/registrar-encuestador/registrar-encuestador.component.ts
@@ -8,10 +8,23 @@ import {ActivatedRoute} from '@angular/router';
 export class RegistrarEncuestadorComponent implements OnInit {
 
       public id: string;
+      public idEncuestador:number;
+      public nombreEncuestador:string;
+      public cedula:number;
   constructor(private route: ActivatedRoute) {
   route.params.subscribe(
     params=>{this.id=params['id']})
   }
+    EnviarDatos(id:string,idEncuestador:number,nombreEncuestador:string,cedula:number){
+    this.id=id;
+    this.idEncuestador=idEncuestador
+    this.nombreEncuestador=nombreEncuestador;
+    this.cedula=cedula;  
+    }
+   
+    MostrarDatos(){
+      return(this.id,this.idEncuestador,this.nombreEncuestador,this.cedula);
+    }
 
   ngOnInit() {
   }
