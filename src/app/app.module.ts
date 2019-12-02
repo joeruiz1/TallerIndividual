a19b97e98b8c074dedfb71b50bf7796ed6091351
joeruiz1 <fernandostiven14yo@gmail.com>
2019-12-01 23:47:04 -0500

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import{RouterModule,Routes}from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
@@ -9,8 +9,20 @@ import { RegistrarEncuestadorComponent } from './registrar-encuestador/registrar
 import { RegistraEncuestaComponent  } from './registrar-encuesta/registrar-encuesta.component';
 import { VisualizarEncuestasComponent } from './visualizar-encuestas/visualizar-encuestas.component';
 
+const appRoutes:Routes=[
+  {path:'',component:MenuPrincipalComponent},
+  {path :'registrar-encuestador',component:RegistrarEncuestadorComponent},
+  {path :'registrar-encuesta',component:RegistraEncuestaComponent},
+  {path :'visualizar-encuestas',component:VisualizarEncuestasComponent}  
+
+]
+
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot(appRoutes)
+   ],
   declarations: [ AppComponent, HelloComponent, MenuPrincipalComponent, RegistraEncuestaComponent , RegistrarEncuestadorComponent, VisualizarEncuestasComponent ],
   bootstrap:    [ AppComponent ]
 })
